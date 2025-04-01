9d99ffbf985402430250f21fcf1386185b1419b6
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2025-04-01 23:03:59 +0800

    Upload combiner

---
+++ csv_combiner.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def combine(file_path):
+    first = pd.read_csv(f"{file_path}/first_partition.csv")
+    second = pd.read_csv(f"{file_path}/second_partition.csv")
+    third = pd.read_csv(f"{file_path}/third_partition.csv")
+    fourth = pd.read_csv(f"{file_path}/fourth_partition.csv")
+    fifth = pd.read_csv(f"{file_path}/fifth_partition.csv")
+    sixth = pd.read_csv(f"{file_path}/sixth_partition.csv")
+    combined = pd.concat([first, second, third, fourth, fifth, sixth])
+    combined.to_csv(f"{file_path}/output.csv", index=False)
+
+if __name__ == "__main__":
+    combine("./results/meta-llama_Llama-3.3-70B-Instruct-Turbo-Free")
+    combine("./results/Gemma2_27b")
